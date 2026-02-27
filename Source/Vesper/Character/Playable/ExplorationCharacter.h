693e9907ace0ddb4cf152876c0199db57aed83a5
MarcelKrawczyk <marcelkrawczyk04@gmail.com>
2026-02-27 04:58:17 +0100

    Fix ExplorationCharacter code clarity

--- Source/Vesper/Character/Playable/ExplorationCharacter.h
+++ Source/Vesper/Character/Playable/ExplorationCharacter.h
@@ -1,5 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
@@ -32,22 +30,17 @@ protected:
 	class UInputAction* LookAction;
 	
 public:
-	// Sets default values for this character's properties
 	AExplorationCharacter();
 
 protected:
-	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
 public:	
-	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
-	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 	
 protected:
-	
 	void Move(const FInputActionValue& InputValue);
 	void Look(const FInputActionValue& InputValue);
 	void Jump();
