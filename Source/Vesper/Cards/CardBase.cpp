862ae2fa55b939cc02b08f9e3464867b3e4f3407
MarcelKrawczyk <marcelkrawczyk04@gmail.com>
2026-03-11 03:35:23 +0100

    Implement EnhancedInput for third person game and SpringArm camera. Add usable blueprints for handling enhanced input

--- Source/Vesper/Cards/CardBase.cpp
+++
@@ -1,6 +0,0 @@
-#include "Vesper/Cards/CardBase.h"
-
-void UCardBase::Execute_Implementation(ABattleCharacter* User, AEnemyCharacter* Target)
-{
-	// DOMYŚLNIE NIC — konkretne karty nadpisują tę funkcję
-}
